c3e2db8caa6301929a8e13b2f08772faa71c4522
nernonaigle <nernonedouard99@gmail.com>
2026-02-23 10:51:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,16 @@ v_url = "https://ryfrekltrgaqyryzozhc.supabase.co"
 v_key = "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0"
 supabase = create_client(v_url, v_key)
 
-st.set_page_config(page_title="AEEMG", page_icon="🤝")
+st.set_page_config(page_title="AEEMG")
 st.title("🤝 AEEMG Association")
 
 menu = st.radio("Navigation", ["Accueil", "Inscription"], horizontal=True)
 
-if menu == "Accueil":
-st.header("Fil d'actualité")
-st.write("Bienvenue sur l'espace officiel de l'AEEMG !")
+if menu == "Accueil": st.write("### Bienvenue sur le fil d'actualité !")
 
 if menu == "Inscription":
-st.header("Devenir membre")
-nom = st.text_input("Nom")
-prenom = st.text_input("Prénom")
-filiere = st.selectbox("Filière", ["Médecine", "Pharmacie", "Odontologie", "Autre"])
-email = st.text_input("Email")
-if st.button("S'inscrire"):
-if nom and prenom and email:
-st.success(f"Merci {prenom} ! Inscription reçue.")
-else:
-st.warning("Remplis tous les champs.")
+st.write("### Formulaire d'inscription")
+nom = st.text_input("Ton Nom")
+prenom = st.text_input("Ton Prénom")
+email = st.text_input("Ton Email")
+if st.button("Valider"): st.success("C'est envoyé !")
