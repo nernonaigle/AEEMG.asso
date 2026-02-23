f9e85860d2cfbb2b4de8e092fa4d47f8bf9cca99
nernonaigle <nernonedouard99@gmail.com>
2026-02-23 00:41:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 from supabase import create_client
 
-v_url = ""
-v_key = "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0"
-
-supabase = create_client(v_url, v_key)
+supabase = create_client("", "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0")
 
 st.title("🤝 AEEMG Association")
+st.write("Connexion établie avec succès !")
 
 tab1, tab2 = st.tabs(["Accueil", "Profil"])
-
-with tab1: st.write("Bienvenue sur le fil d'actualité.")
-
-with tab2: st.write("Page de profil.")
+with tab1: st.write("Contenu de l'accueil")
+with tab2: st.write("Contenu du profil")
