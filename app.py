ba6475e7ae3b61c3c447d2c750fccff3c44ff4b2
nernonaigle <nernonedouard99@gmail.com>
2026-02-23 08:30:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 from supabase import create_client
 
-On remplit bien les deux zones entre guillemets :
-url_supabase = ""
-cle_supabase = "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0"
+1. Tes clés de connexion
+URL = "https://ryfrekltrgaqyryzozhc.supabase.co"
+KEY = "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0"
 
-Connexion
-supabase = create_client(url_supabase, cle_supabase)
+2. Configuration de la page
+st.set_page_config(page_title="AEEMG", page_icon="🤝")
 
+3. Connexion et Affichage
+try:
+supabase = create_client(URL, KEY)
 st.title("🤝 AEEMG Association")
-st.success("Connexion réussie !")
+st.success("Bravo ! Ton application est connectée.")
 
-tab1, tab2 = st.tabs(["Accueil", "Profil"])
-with tab1: st.write("Bienvenue sur le fil d'actualité.")
-with tab2: st.write("Page de profil.")
+except Exception as e:
+st.error(f"Il y a une petite erreur : {e}")
