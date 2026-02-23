3e3f80768ad0bc269bf2a072728497d50fbc9299
nernonaigle <nernonedouard99@gmail.com>
2026-02-23 00:42:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from supabase import create_client
 supabase = create_client("", "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0")
 
 st.title("🤝 AEEMG Association")
-st.write("Connexion établie avec succès !")
+st.success("Bravo ! La connexion est enfin établie.")
 
 tab1, tab2 = st.tabs(["Accueil", "Profil"])
-with tab1: st.write("Contenu de l'accueil")
-with tab2: st.write("Contenu du profil")
+with tab1: st.write("Bienvenue sur le fil d'actualité de l'AEEMG.")
+with tab2: st.write("Ici, tu pourras modifier ton profil.")
