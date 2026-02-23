c70ea8101224b304289381ec62aad7d3fa7bc7fa
nernonaigle <nernonedouard99@gmail.com>
2026-02-23 10:49:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from supabase import create_client
 
-Connexion
 v_url = "https://ryfrekltrgaqyryzozhc.supabase.co"
 v_key = "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0"
 supabase = create_client(v_url, v_key)
@@ -9,11 +8,20 @@ supabase = create_client(v_url, v_key)
 st.set_page_config(page_title="AEEMG", page_icon="🤝")
 st.title("🤝 AEEMG Association")
 
-t1, t2 = st.tabs(["Accueil", "Inscription"])
+menu = st.radio("Navigation", ["Accueil", "Inscription"], horizontal=True)
 
-with t1:
+if menu == "Accueil":
 st.header("Fil d'actualité")
 st.write("Bienvenue sur l'espace officiel de l'AEEMG !")
 
-with t2:
+if menu == "Inscription":
 st.header("Devenir membre")
+nom = st.text_input("Nom")
+prenom = st.text_input("Prénom")
+filiere = st.selectbox("Filière", ["Médecine", "Pharmacie", "Odontologie", "Autre"])
+email = st.text_input("Email")
+if st.button("S'inscrire"):
+if nom and prenom and email:
+st.success(f"Merci {prenom} ! Inscription reçue.")
+else:
+st.warning("Remplis tous les champs.")
