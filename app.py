439de0378d65995f323cdcb48fec7e55c6b36a74
nernonaigle <nernonedouard99@gmail.com>
2026-02-23 08:33:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ from supabase import create_client
 v_url = "https://ryfrekltrgaqyryzozhc.supabase.co"
 v_key = "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0"
 
-try:
 supabase = create_client(v_url, v_key)
+
 st.set_page_config(page_title="AEEMG")
 st.title("🤝 AEEMG Association")
 st.success("Bravo ! Connexion réussie.")
 
-except Exception as e:
-st.error(f"Erreur : {e}")
+t1, t2 = st.tabs(["Accueil", "Profil"])
+
+with t1:
+st.write("Bienvenue sur le fil d'actualité.")
+
+with t2:
+st.write("Ceci est la page de profil.")
