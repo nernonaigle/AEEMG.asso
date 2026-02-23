d9cf1a307bf98bca555072086955ac19836c00af
nernonaigle <nernonedouard99@gmail.com>
2026-02-23 10:46:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from supabase import create_client
 
 Connexion
-v_url = ""
+v_url = "https://ryfrekltrgaqyryzozhc.supabase.co"
 v_key = "sb_publishable_iYEJIAz8ZK-fls3KMXI-pw_gcyinvF0"
 supabase = create_client(v_url, v_key)
 
